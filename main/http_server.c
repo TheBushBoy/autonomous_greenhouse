81664ef6072e0047cc541f5e2a1f9107f10346e7
TheBushBoy <theodavid@icloud.com>
2026-02-04 13:59:39 +0100

    Use const not static for uri handler

--- main/http_server.c
+++ main/http_server.c
@@ -37,19 +37,20 @@ static esp_err_t sensors_get_handler(httpd_req_t* req) {
     return ESP_OK;
 }
 
-static const httpd_uri_t uri_get_sensors = {
-    .uri       = "/api/sensors",
-    .method    = HTTP_GET,
-    .handler   = sensors_get_handler,
-    .user_ctx  = NULL
-};
-
 httpd_handle_t start_webserver(void) {
     httpd_handle_t server = NULL;
     httpd_config_t config = HTTPD_DEFAULT_CONFIG();
     config.lru_purge_enable = true;
 
     ESP_LOGI(TAG, "HTTP server on port: '%d'", config.server_port);
+
+    const httpd_uri_t uri_get_sensors = {
+        .uri       = "/api/sensors",
+        .method    = HTTP_GET,
+        .handler   = sensors_get_handler,
+        .user_ctx  = NULL
+    };
+
     if (httpd_start(&server, &config) == ESP_OK) {
         httpd_register_uri_handler(server, &uri_get_sensors);
         return server;
